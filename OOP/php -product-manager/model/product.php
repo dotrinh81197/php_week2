252eaa4a8e24ec43a61d0047803d05cbd04638cd
dotrinh81197 <dotrinh81197@users.noreply.github.com>
2020-12-02 15:35:43 +0700

    quan ly san pham

---
+++ OOP/php -product-manager/model/product.php
@@ -0,0 +1,27 @@
+<?php
+class Product
+{
+    private $name;
+    private $price;
+    function __construct($name = NULL, $price = 0)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+    function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
